d2986855edd5d824524ea20ae2a135505d7b9e79
fronre <islamhala35@gmail.com>
2025-11-03 22:58:08 +0100

    add function tri_insertion_deplacement.py and add function tri_insertion_echange.py

---
+++ algos/tri_insertion_deplacement.py
@@ -0,0 +1,18 @@
+from typing import List
+
+def tri_insertion_deplacement(arr: List[int]) -> tuple[list[int], int, int]:
+    a = arr.copy()
+    comparaisons = 0
+    deplacements = 0
+
+    for i in range(1, len(a)):
+        key = a[i]
+        j = i - 1
+        while j >= 0 and a[j] > key:
+            comparaisons += 1
+            a[j + 1] = a[j]
+            j -= 1
+            deplacements += 1
+        comparaisons += 1  # pour la dernière comparaison fausse
+        a[j + 1] = key
+    return a, comparaisons, deplacements
--- algos/tri_insertion_echange.py
+++ algos/tri_insertion_echange.py
@@ -1,1 +1,18 @@
-"""
+from typing import List
+
+def tri_insertion_echange(arr: List[int]) -> tuple[list[int], int, int]:
+    a = arr.copy()
+    comparaisons = 0
+    deplacements = 0
+
+    for i in range(1, len(a)):
+        j = i
+        while j > 0:
+            comparaisons += 1
+            if a[j - 1] > a[j]:
+                a[j - 1], a[j] = a[j], a[j - 1]
+                deplacements += 1
+                j -= 1
+            else:
+                break
+    return a, comparaisons, deplacements
