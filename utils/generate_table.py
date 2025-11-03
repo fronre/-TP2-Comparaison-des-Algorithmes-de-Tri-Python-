f01a1c6c94d713d132cd1a76f504ecc6ed6e9161
fronre <islamhala35@gmail.com>
2025-11-03 22:49:05 +0100

    add generate_table.py

---
+++ utils/generate_table.py
@@ -0,0 +1,11 @@
+import random
+
+def generer_table(n: int, dis: str) -> list[int]:
+    if dis == 'c':
+        return list(range(1, n + 1))
+    elif dis == 'd':
+        return list(range(n, 0, -1))
+    elif dis == 'a':
+        return random.sample(range(1, n + 1), n)
+    else:
+        raise ValueError("dis doit être 'c', 'd' ou 'a'")
